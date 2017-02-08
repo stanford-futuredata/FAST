e9337282219667e730bbc9dcc6ff8c821311c43f
Kexin Rong <kexinrongg@gmail.com>
2017-02-08 10:58:22 -0800

    Minor clean up

--- FAST-new/main.cpp
+++ FAST-new/main.cpp
@@ -3,30 +3,57 @@
 #include <iostream>
 #include <fstream>
 #include "boost/dynamic_bitset.hpp"
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
-	uint64_t ntbls = 100;
-	int nhashfuncs = 5;
-	//int nhashfuncs = 8;
-	int ncols = 86381;
-	//uint64_t ncols = 26496002;
-	int mrows = 4096;
-	uint32_t seed = 1783793664;
-	int ntimes = ntbls * nhashfuncs;    //number of min-hash permutations
-	int near_repeats = 5;
-	uint32_t limit = 4e9;
-	int nvotes = 4;
+const int batch_size = 100000;
+uint64_t ntbls = 100;
+int nhashfuncs = 5;
+//int nhashfuncs = 8;
+int ncols = 86381;
+//uint64_t ncols = 26496002;
+int mrows = 4096;
+uint32_t seed = 1783793664;
+int ntimes = ntbls * nhashfuncs;    //number of min-hash permutations
+int near_repeats = 5;
+uint32_t limit = 4e9;
+int nvotes = 4;
+const int byte_per_fp = mrows / 8;
 
-	// Compute hashes for each row
+
+void OutputHashTableStats(table* t) {
+	vec maxItemsInBucket;
+	vec bucketsPerTable = CountBucketsPerTable(ntbls, t, &maxItemsInBucket);
+	ofstream ofile ("stats/overview.txt");
+	for (int i = 0; i < ntbls; i ++) {
+		ofile << i << "," << bucketsPerTable.at(i) << ","
+			<< maxItemsInBucket.at(i) << endl;
+	}
+	ofile.close();
+
+	for (int i = 0; i < ntbls; i ++) {
+		vec numItemsPerBucket = CountBucketItems(&t[i]);
+		ofstream ofile ("stats/table" + to_string(i) + ".txt" );
+		for (vec_cit it = numItemsPerBucket.begin(); it != numItemsPerBucket.end();
+			it++ ) {
+			ofile << *it << endl;
+		}
+		ofile.close();
+	}
+}
+
+int main(int argc, char * argv[]) {
+    // Compute hashes for each row
 	uint32_t *hashes = (uint32_t*) malloc(ntimes * mrows * sizeof(uint32_t));
 	calculate_hash(mrows, ntimes, seed, hashes);
+
 	// Storage for min-hash signatures
 	const uint64_t sig_len = ncols * ntbls;
 	uint64_t *min_hash_sigs = new uint64_t[sig_len];
+
 	// Fingerprint storage
-	const int batch_size = 100000;
 	boost::dynamic_bitset<> fingerprints(batch_size * mrows);
 	//Fingerprint file
 	ifstream infile;
@@ -35,7 +62,7 @@ int main(int argc, char * argv[]) {
 	uint64_t fp_index = 0;
 	double out_time = 0;
 	int j = 0;
-	const int byte_per_fp = mrows / 8;
+
 	// Reading fingerprints and compute minhashes in batches
 	while (!infile.eof()) {
 		int count = 0;
@@ -44,6 +71,9 @@ int main(int argc, char * argv[]) {
 			char x;
 			infile.read(&x, 1);
 			for (int i = 0; i < 8; i ++) {
+				/*if (rand() % 4096 < 800) {
+					fingerprints[count] = 1;
+				}*/
 				fingerprints[count] = (x >> (7 - i)) & 1;
 				count ++;
 			}
@@ -70,6 +100,10 @@ int main(int argc, char * argv[]) {
 		nvotes, limit, &out_time);
 	cout << "Search took: " << out_time << endl;
 
+	// Hash table stats
+	OutputHashTableStats(t);
+
+	// Clean up
 	ClearDatabase(ntbls, t);
 	delete[] min_hash_sigs;
 	delete[] query;
--- FAST-new/mat-to-txt.py
+++ FAST-new/mat-to-txt.py
@@ -1,11 +1,17 @@
 import scipy.io as sio
 import h5py
 import numpy as np
+import sys
 
-#mat = h5py.File("binaryFingerprint_wLen10_wLag0.1_fpLen100_fpLag10_tvalue800.mat", "r")
-mat = sio.loadmat("binaryFingerprint_wLen10_wLag0.1_fpLen100_fpLag10_tvalue800.mat")
-(x, y) = mat['binaryFingerprint'].shape
-print x, y
-fp = np.array(mat['binaryFingerprint']).T
-b = np.packbits(fp)
-b.astype('int8').tofile("24hr.bin")
+if __name__ == '__main__':
+	fname = sys.argv[1]
+	try:
+		mat = sio.loadmat(fname)
+	except:
+		mat = h5py.File(fname, "r")
+	(x, y) = mat['binaryFingerprint'].shape
+	fp = np.array(mat['binaryFingerprint'])
+	if x < y:
+		fp = fp.T
+	b = np.packbits(fp)
+	b.astype('int8').tofile("fp.bin")
