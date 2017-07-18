38861dea9c4835d7577578b1e0b47e1989d0fe98
Hashem Elezabi <shoma98w@gmail.com>
2017-07-18 16:48:10 -0700

    Add code to map pairs into groups (#1)
    * First version of analyze_pairs.py (in addition to sort_pairs.py)
    
    * Version 2 of analyze_pairs. Plots are working, but no desired diagonal result yet
    
    * Added a few plots and result group text files
    
    * Version 3. analyze_pairs writes a summary of each group to an output file, the algorithm has been modified, and visualization now depends on a flag
    
    * Faster non-parallel analyze_pairs, parse and sort combined
    
    * Parallel version working, analyze takes command line arguments for file path, sort_pairs.py removed, plot_time_series made into a separate script
    
    * Remove kurt filter file
    
    * Merge parallel and non-parallel versions of analyze, add comments, separate main code from visualization code
    
    * Remove extraneous files

---
+++ pyFAST/postprocessing/scripts/parse_results.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+from operator import itemgetter
+
+if __name__ == '__main__':
+	fname = sys.argv[1]
+	a = np.fromfile(fname, dtype=np.uint32)
+	i = 0
+	pairs = []
+	while (i < len(a)):
+		idx = a[i]
+		i += 1
+		counts = a[i]
+		i += 1
+		for j in range(counts / 2):
+			pairs.append((idx, a[i + j * 2], a[i + j * 2 + 1]))
+		i += counts
+	pairs.sort(key=itemgetter(0, 1))
+	f = open("result_pairs.txt", 'w')
+	for i in xrange(len(pairs)):
+		f.write("%d %d %d\n" % (pairs[i][0], pairs[i][1], pairs[i][2]))
+	f.close()
