3de5ce05705917bce2bf0b244913282289d47b62
Kexin Rong <kexinrongg@gmail.com>
2017-07-21 17:01:34 -0700

    Add script to read fingerprints

--- pyFAST/fingerprint/finger_print.py
+++ pyFAST/fingerprint/finger_print.py
@@ -40,8 +40,8 @@ if __name__ == '__main__':
 	init_MAD_stats(mad_fname)
 
 	# Create timestamp and fingerprint folder if not exist
-	fp_folder = params.data_folder % params.station + 'fingerprints/'
-	ts_folder = params.data_folder % params.station + 'timestamps/'
+	fp_folder = params.data_folder % params.station + 'new_fingerprints/'
+	ts_folder = params.data_folder % params.station + 'new_timestamps/'
 	if not os.path.exists(fp_folder):
 		os.makedirs(fp_folder)
 	if not os.path.exists(ts_folder):
@@ -66,6 +66,7 @@ if __name__ == '__main__':
 		s = starttime
 		# Generate and output fingerprints per partition_len
 		while endtime - s > datetime.timedelta(seconds = params.min_fp_length):
+            e = min(s + params.partition_len, endtime)
 			e_extra = min(s + params.partition_len + time_extra, endtime)
 			partition_st = st[i].slice(UTCDateTime(s.strftime('%Y-%m-%dT%H:%M:%S.%f')),
 				UTCDateTime(e_extra.strftime('%Y-%m-%dT%H:%M:%S.%f')))
