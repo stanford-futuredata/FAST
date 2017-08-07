3d57eb8512ce0a198e198ef754309a7221d50a11
Kexin Rong <kexinrongg@gmail.com>
2017-08-07 01:19:51 -0700

    Move parameters to json; change filename construction => all files under folder

--- pyFAST/fingerprint/MAD.py
+++ pyFAST/fingerprint/MAD.py
@@ -5,10 +5,8 @@ from obspy.core import UTCDateTime
 import os
 import sys
 import random
-import params
 import datetime
-from params import construct_filename
-from feature_extractor import FeatureExtractor
+from util import *
 
 def get_start_end_time(st, idx):
 	starttime = datetime.datetime.strptime(str(st[idx].stats.starttime), '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -27,55 +25,52 @@ def get_segment(st, starttime, endtime, sample_length):
 
 
 def get_haar_stats(start_time, end_time):
-	time = start_time
-	sample_haar_images = np.zeros([0, int(params.nfreq) * int(params.ntimes)])
-	while time < end_time:
-		fname = construct_filename(time, station)
-		st = read('%s%s' %((params.data_folder % station), fname))
+	p = params['fingerprint']
+	min_fp_length = get_min_fp_length(params)
+	sample_haar_images = np.zeros([0, 
+		int(p['nfreq']) * int(p['ntimes'])])
+	files = get_data_files(params)
+	for fname in files:
+		st = read(params['data']['folder'] + fname)
 		# No sampling
-		if params.sampling_rate == 1:
+		if p['mad_sampling_rate'] == 1:
 			for i in range(len(st)):
 				starttime, endtime = get_start_end_time(st, i)
-				if endtime - starttime <= datetime.timedelta(seconds=params.min_fp_length):
+				if endtime - starttime <= datetime.timedelta(seconds=min_fp_length):
 					print 'continue'
 					continue
 				haar_images, nWindows, idx1, idx2, Sxx, t = feats.data_to_haar_images(st[i].data)
 				sample_haar_images = np.vstack([sample_haar_images, haar_images])
 		# Randomly sample sample_length proportion of data from each input file
 		else:
-			sample_length = max((time + params.INTERVAL - time).total_seconds() 
-				* params.sampling_rate, params.min_fp_length)
+			sample_length = max((st[len(st) - 1].stats.endtime - st[0].stats.starttime) * \
+				p['mad_sampling_rate'], min_fp_length)
 			# Get a random trace that is long enough
 			idx = random.randint(0, len(st) - 1)
 			starttime, endtime = get_start_end_time(st, idx)
-			while sample_length >= params.min_fp_length:
+			while sample_length >= min_fp_length:
 				# Get time series sample
 				trace_length = (endtime - starttime).total_seconds()
-				if trace_length > params.min_fp_length:
+				if trace_length > min_fp_length:
 					segment = get_segment(st[idx], starttime, endtime, min(trace_length, sample_length))
 					haar_images, nWindows, idx1, idx2, Sxx, t = feats.data_to_haar_images(segment.data)
 					sample_haar_images = np.vstack([sample_haar_images, haar_images])
 					sample_length -= min(sample_length, trace_length)
 				idx = random.randint(0, len(st) - 1)
 				starttime, endtime = get_start_end_time(st, idx)
-		time += params.INTERVAL
 
 	return feats.compute_haar_stats(sample_haar_images)
 
 
 if __name__ == '__main__':
-	start_time = datetime.datetime.strptime(sys.argv[1], "%y-%m")
-	end_time = datetime.datetime.strptime(sys.argv[2], "%y-%m")
-	station = sys.argv[3]
-
-	feats = FeatureExtractor(sampling_rate=params.Fs, window_length=params.spec_length, 
-		window_lag=params.spec_lag, fingerprint_length=params.fp_length, 
-		fingerprint_lag=params.fp_lag, min_freq=params.fmin, max_freq=params.fmax)
+	param_json = sys.argv[1]
+	params = parse_json(param_json)
+	start_time, end_time = get_start_end_times(params)
+	feats = init_feature_extractor(params)
 
 	median, mad = get_haar_stats(start_time, end_time)
 	# Output MAD stats to file
-	f = open('mad%s-%s-%s%s.txt' % (
-		station, params.channel, sys.argv[1], sys.argv[2]), 'w')
+	f = open(gen_mad_fname(params), 'w')
 	for i in range(len(median)):
 		f.write('%.16f,%.16f\n' %(median[i], mad[i]))
 	f.close()
---
+++ pyFAST/fingerprint/util.py
@@ -0,0 +1,52 @@
+import json
+import datetime
+from os import listdir
+from os.path import isfile, join, abspath, dirname
+from feature_extractor import FeatureExtractor
+
+def parse_json(param_json):
+    with open(param_json) as json_data_file:
+        params = json.load(json_data_file)
+    return params
+
+def should_include_file(f, params):
+    return params['data']['station'] in f and \
+        params['data']['channel'] in f
+
+def get_data_files(params):
+    path = abspath(join(dirname(__file__), params['data']['folder']))
+    files = []
+    for f in listdir(path):
+        if isfile(join(path, f)) and should_include_file(f, params):
+            #files.append(join(path, f))
+            files.append(f)
+    return files
+
+def get_min_fp_length(params):
+    return params['fingerprint']['fp_length'] * params['fingerprint']['spec_lag'] + \
+        params['fingerprint']['spec_length']
+
+def get_start_end_times(params):
+    start_time = datetime.datetime.strptime(params['data']['start_time'], 
+        "%y-%m-%dT%H:%M:%S.%f")
+    end_time = datetime.datetime.strptime(params['data']['end_time'], 
+        "%y-%m-%dT%H:%M:%S.%f")
+    return (start_time, end_time)
+
+def gen_mad_fname(params):
+    return 'mad%s_%s_%s_%s.txt' % (
+        params['data']['station'],
+        params['data']['channel'],
+        params['data']['start_time'],
+        params['data']['end_time'] )
+
+def init_feature_extractor(params):
+    feats = FeatureExtractor(sampling_rate=params['fingerprint']['sampling_rate'],
+        window_length=params['fingerprint']['spec_length'],
+        window_lag=params['fingerprint']['spec_lag'],
+        fingerprint_length=params['fingerprint']['fp_length'],
+        fingerprint_lag=params['fingerprint']['fp_lag'],
+        min_freq=params['fingerprint']["min_freq"],
+        max_freq=params['fingerprint']["max_freq"])
+    return feats
+
