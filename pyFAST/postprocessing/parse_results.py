aa80178dd30c459532e56a3e2acd8739f13eb9f0
hashemelezabi <shoma98w@gmail.com>
2017-10-31 21:37:50 -0700

    Parse in parallel with parse_chunk and get_positions

--- pyFAST/postprocessing/parse_results.py
+++ pyFAST/postprocessing/parse_results.py
@@ -6,6 +6,8 @@ from os.path import isfile, join
 import multiprocessing
 import argparse
 import subprocess
+import time
+import struct
 
 MB_TO_BYTES = 1024 * 1024
 
@@ -58,8 +60,76 @@ def _output_buffer(buf, f_out):
             lines.append("%s %d\n" % (elem[0], elem[1]))
     f_out.writelines(lines)
 
+def get_positions(fname):
+    print "Getting positions %s" % fname
+    start = time.time()
+    # File size in bytes
+    file_size = os.path.getsize(fname)
+    with open(fname, 'r') as f:
+        pos_list = [] # list of positions of idx values, in bytes
+        pointer = 4 # 5th byte, second uint32, first count
+        while pointer < file_size:
+            pos_list.append(pointer - 4) # count position minus 1 byte gives position of idx
+            f.seek(pointer)
+            count = struct.unpack('I', f.read(4))[0] # read uint32 at current position
+            pointer += (count + 2) * 4 # jump to the next count
+    final_list = []
+    first_pos = pos_list[0]
+    prev_pos = pos_list[0]
+    for pos in pos_list:
+        if pos - first_pos > partition_memory:
+            final_list.append((fname, first_pos, prev_pos))
+            first_pos = pos
+        prev_pos = pos
+    final_list.append((fname, first_pos, prev_pos))
+    print "Time to get positions:", time.time() - start
+    return final_list
+
+def parse_chunk(tup):
+    filename = tup[0]
+    first_pos = tup[1]
+    last_pos = tup[2]
+    print "Parsing %s at (%d,%d)" % (filename, first_pos, last_pos)
+    start = time.time()
+    pairs_file_name = _get_pairs_fname("%s_(%d,%d)" % (filename, first_pos, last_pos))
+    pairs_file = open(pairs_file_name, 'w')
+    with open(filename, 'r') as f:
+        f.seek(last_pos + 4) # seek the position of the last count
+        last_count = struct.unpack('I', f.read(4))[0]
+        read_size = last_pos + (last_count + 2) * 4 - first_pos # size of chunk to read in bytes
+        f.seek(first_pos)
+        buf = f.read(read_size)
+        a = np.frombuffer(buf, dtype=np.uint32)
+        i = 0
+        lines = []
+        while i < len(a):
+            idx = a[i]
+            i += 1
+            counts = a[i]
+            i += 1
+            if args.idx:
+                global_idx = idx_map[idx]
+            for j in range(counts / 2):
+                # Only add things that are above specified thresholds
+                sim = a[i + j * 2 + 1]
+                idx2 = a[i + j * 2]
+                if args.thresh is None or sim >= args.thresh:
+                    # Map station fingerprint index to global index
+                    if args.idx:
+                        lines.append('%d %d %d\n' %(
+                            abs(idx_map[idx2] - global_idx), max(idx_map[idx2], global_idx), sim))
+                    # Use station index
+                    else:
+                        lines.append('%d %d %d\n' %(abs(idx2 - idx), max(idx2, idx), sim))
+            i += counts
+    pairs_file.writelines(lines)
+    pairs_file.close()
+    print "Time to parse %s at (%d,%d):" % (filename, first_pos, last_pos), time.time() - start
+    return pairs_file_name
+
 def parse_partition(fname):
     print "Parsing %s" % fname
+    start = time.time()
     # File size in bytes
     file_size = os.path.getsize(fname)
     offset = 0
@@ -108,24 +178,29 @@ def parse_partition(fname):
 
     pairs_file.close()
     f.close()
+    print "Time to parse %s:" % fname, time.time() - start
 
 def sort(fname):
     print "Sorting %s" % fname
+    start = time.time()
     subprocess.call(['sort', '-k1,1n', '-k2,2n', '-o', fname + '_sorted', fname])
-    print "Done sorting %s" % fname
+    print "Done sorting %s, time taken:" % fname, time.time() - start
     os.remove(fname)
 
 def merge(sorted_filenames):
     print "Merging files"
+    start = time.time()
     subprocess.call(
         ['sort', '-m'] + sorted_filenames + \
         ['-k1,1n', '-k2,2n', '-o', '%s%s_merged.txt' % (args.dir, args.prefix)])
     map(lambda f: os.remove(f), sorted_filenames)
+    print "Done merging, time taken:", time.time() - start
 
 ''' Add up similarity and filter out those below threshold '''
 def filter_and_reduce():
     print "Filtering by threshold, outputing results to %s%s_combined.txt" \
         % (args.dir, args.prefix)
+    start = time.time()
     f_out = open('%s%s_combined.txt' % (args.dir, args.prefix), 'w')
     with open('%s%s_merged.txt' % (args.dir, args.prefix), 'r') as f:
         buf = []
@@ -151,7 +226,8 @@ def filter_and_reduce():
         _output_buffer(buf, f_out)
     f_out.close()
     # Remove intermediate file
-    os.remove('%s_merged.txt' % args.prefix)
+    os.remove('%s%s_merged.txt' % (args.dir, args.prefix))
+    print "Done filtering, time taken:", time.time() - start
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -186,6 +262,8 @@ if __name__ == '__main__':
                         help='whether to combine similarity for the same pair')
     args = parser.parse_args()
 
+    grand_start_time = time.time()
+
     idx_map = {}
     if args.idx:
         idx_map = get_global_index_map(args.idx)
@@ -210,5 +288,24 @@ if __name__ == '__main__':
     merge(out_fnames)
     if args.combine:
         filter_and_reduce()
+
+    # partition_memory = parse_memory(args.mem) / len(fnames) / len(fnames)
+    # p = multiprocessing.Pool(len(fnames) * len(fnames))
+    # out_fnames = fnames
+    # if args.parse:
+    #     file_chunks = p.map(get_positions, fnames) # list of lists, where each list is a list of tuples of the form (fname, first_pos, last_pos)
+    #     file_chunks = [chunk for chunk_list in file_chunks for chunk in chunk_list] # flatten the list
+    #     # Parse each binary output partition
+    #     out_fnames = p.map(parse_chunk, file_chunks)
+    # # Sort each partition
+    # if args.sort:
+    #     p.map(sort, out_fnames)
+    #     out_fnames = map(_get_sorted_fname, out_fnames)
+    # # Merge sorted partitinos
+    # merge(out_fnames)
+    # if args.combine:
+    #     filter_and_reduce()
+
+    print "Total time taken:", time.time() - grand_start_time
 
 
