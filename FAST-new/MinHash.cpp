03595b46d0f24a3cc0782efbde9a5d62a946fff9
Kexin Rong <kexinrongg@gmail.com>
2018-01-05 21:49:22 -0800

    Minhash w/ bad locality

--- FAST-new/MinHash.cpp
+++ FAST-new/MinHash.cpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// 
+//
 #include "MinHash.h"
 #include "MurmurHash3.h"
 #include <limits.h>
@@ -16,54 +16,43 @@ void calculate_hash(int ny, int ntimes, uint32_t seed, uint32_t* hashes) {
       MurmurHash3_x86_32( &i, 4, seed + j, &hashes[j + i * ntimes]);
 }
 
-void MinHashMM_Block_32(const boost::dynamic_bitset<> &block, int nx, int ny, int ntbls,
-  int ntotfunc, const uint32_t* hashes, uint64_t *out, uint64_t fp_index) {
+void MinHashMM_Block_32(const boost::dynamic_bitset<> &block, int nx, int ny, int ntbls, int ntotfunc,
+  const uint32_t* hashes, void *out, uint64_t fp_index){
+
+   // nnz_max  : Maximum number of non-zero bits
+   int nnz_max = ny;
+   int ntimes = ntbls * ntotfunc;
+   uint32_t *indices = (uint32_t*)calloc(nnz_max,sizeof(uint32_t));
 
    //For each fingerprint
-   int nhashfuncs = (ntotfunc + 1) / 2;
-   int ntimes = ntbls * nhashfuncs;
-   uint64_t block_index = 0;
-   uint64_t offset = fp_index;
-   uint32_t *minhash = (uint32_t *) calloc(ntimes, sizeof(uint32_t));
-   uint32_t *maxhash = (uint32_t *) calloc(ntimes, sizeof(uint32_t));
-   for (int i = 0; i < ntimes; i ++) {
-      minhash[i] = UINT_MAX;
-   }
-
-   for (int i = 0; i < nx; ++i) {
+   for( int i = 0; i != nx; ++i ) {
        //Locate non-zero entries
-       for (int j = 0; j != ny; ++j) {
-           if ((bool)block[block_index]) {
-                for (int k =0; k < ntimes; k ++) {
-                  minhash[k] = std::min(minhash[k],
-                    hashes[k + j * ntimes]);
-                  maxhash[k] = std::max(maxhash[k],
-                    hashes[k + j * ntimes]);
-                }
+       int nnz = 0;
+       for( int j = 0; j != ny; ++j){
+           //assert(j + ny*i <= nx*ny,"e1");
+           //if( block[j + ny*i] == 1){
+           uint64_t temp_index = static_cast<uint64_t>(j) + static_cast<uint64_t>(ny)*static_cast<uint64_t>(i);
+           if((bool)block[temp_index]){
+               indices[nnz] = j;
+               nnz++;
            }
-           block_index ++;
        }
-
-       for (int j = 0; j < ntbls; j ++ ) {
-          size_t key = 0;
-          for (int k = 0; k < ntotfunc; k ++) {
-            if (k < nhashfuncs) {
-              boost::hash_combine(key, minhash[nhashfuncs * j + k]);
-            } else {
-              boost::hash_combine(key, maxhash[nhashfuncs * j + k - nhashfuncs]);
+       //Generate min-Hash signature
+       for( int j = 0; j != ntimes; ++j){
+            uint32_t imin = UINT_MAX; 
+            for( int k = 0; k != nnz; ++k) {
+                int block_index = indices[k] + j * nnz_max;
+                if( hashes[block_index] < imin )
+                    imin = hashes[block_index];
             }
-          }
-          out[offset] = key;
-          offset += 1;
-       }
-       for (int k = 0; k < ntimes; k++) {
-          minhash[k] = UINT_MAX;
-          maxhash[k] = 0;
+            uint64_t temp_index = static_cast<uint64_t>(j) + static_cast<uint64_t>(i)*static_cast<uint64_t>(ntimes);
+            ((uint8_t*)out)[temp_index] = imin % 255;
        }
    } //End for each fingerprint
 
-   free(minhash);
-   free(maxhash);
+   free(indices);
+
 }
+
 
 
