e40bdeb2ec81b1e1a6fe1d0d316e06304bd7e00d
Kexin Rong <kexinrongg@gmail.com>
2018-01-05 16:09:08 -0800

    MinHash

--- FAST-new/MinHash.cpp
+++ FAST-new/MinHash.cpp
@@ -16,7 +16,49 @@ void calculate_hash(int ny, int ntimes, uint32_t seed, uint32_t* hashes) {
       MurmurHash3_x86_32( &i, 4, seed + j, &hashes[j + i * ntimes]);
 }
 
+
 void MinHashMM_Block_32(const boost::dynamic_bitset<> &block, int nx, int ny, int ntbls,
+    int ntotfunc, const uint32_t* hashes, uint64_t *out, uint64_t fp_index) {
+
+   //For each fingerprint
+   uint64_t block_index = 0;
+   int ntimes = ntbls * ntotfunc;
+   uint32_t *minhash = (uint32_t *) calloc(ntimes, sizeof(uint32_t));
+   for (int i = 0; i < ntimes; i ++) {
+      minhash[i] = UINT_MAX;
+   }
+   uint64_t offset = fp_index;
+
+   for (int i = 0; i != nx; ++i) {
+       //Locate non-zero entries
+       for (int j = 0; j != ny; ++j) {
+           if ((bool)(block)[block_index]) {
+                for (int k =0; k < ntimes; k ++) {
+                  minhash[k] = std::min(minhash[k],
+                    hashes[k + j * ntimes]);
+                }
+           }
+           block_index ++;
+       }
+
+       for (int j = 0; j < ntbls; j ++ ) {
+          size_t key = 0;
+          for (int k = 0; k < ntotfunc; k ++) {
+            boost::hash_combine(key, minhash[ntotfunc * j + k]);
+          }
+          out[offset] = key;
+          offset += 1;
+       }
+       for (int k = 0; k < ntimes; k++) {
+          minhash[k] = UINT_MAX;
+       }
+
+   } //End for each fingerprint
+
+   free(minhash);
+}
+
+void MinMaxHashMM_Block_32(const boost::dynamic_bitset<> &block, int nx, int ny, int ntbls,
   int ntotfunc, const uint32_t* hashes, uint64_t *out, uint64_t fp_index) {
 
    //For each fingerprint
