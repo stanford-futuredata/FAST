1b636a927c02e576f25f0c08a9ef56f79d34816d
ceyoon <ceyoon@dawn9.stanford.edu>
2018-06-13 18:38:26 -0700

    Cleaned up preprocessing scripts to fill time gaps, bandpass filter and decimate; added preprocess parameter folder for bandpass filter and decimate

---
+++ parameters/preprocess/ItalyDayYR/bandpass_filter_decimate_ItalyDayYR.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python bandpass_filter_decimate.py YR ED01 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED02 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED03 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED04 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED05 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED06 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED07 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED09 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED10 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED11 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED12 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED14 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED15 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED16 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED17 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED18 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED19 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED20 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED21 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED23 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED24 HHZ 2 20 4
+python bandpass_filter_decimate.py YR ED25 HHZ 2 20 4
