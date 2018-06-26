9d8dceb1e9a48803786d84bead830354b09e8273
ceyoon <ceyoon@dawn7.stanford.edu>
2018-06-25 20:12:54 -0700

    Updated preprocess fill bandpass filter for OBSTest data set

---
+++ parameters/preprocess/OBSTest/bandpass_filter_decimate_OBSTest.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+python bandpass_filter_decimate.py 2D OBS01 SXZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS01 SXH 4 25 1
+
+python bandstop_filter_decimate.py 2D OBS02 SXZ 3 9 1
+python bandpass_filter_decimate.py 2D OBS02 SXH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS03 BXZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS03 LXH 4 25 1
+
+python bandstop_filter_decimate.py 2D OBS05 SXZ 3 9 1
+python bandpass_filter_decimate.py 2D OBS05 SXH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS06 BXZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS06 LXH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS07 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS07 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS08 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS08 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS10 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS10 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS11 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS11 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS13 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS13 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS15 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS15 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS17 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS17 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS18 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS18 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS19 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS19 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS20 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS20 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS21 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS21 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS22 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS22 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS23 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS23 LDH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS24 BHZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS24 LDH 4 25 1
+
+python bandstop_filter_decimate.py 2D OBS26 SXZ 3 9 1
+python bandpass_filter_decimate.py 2D OBS26 SXH 4 16 1
+
+python bandstop_filter_decimate.py 2D OBS27 SXZ 3 9 1
+python bandpass_filter_decimate.py 2D OBS27 SXH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS28 BXZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS28 LXH 4 25 1
+
+python bandstop_filter_decimate.py 2D OBS29 SXZ 3 9 1
+python bandpass_filter_decimate.py 2D OBS29 SXH 4 25 1
+
+python bandstop_filter_decimate.py 2D OBS30 SXZ 3 12 1
+python bandpass_filter_decimate.py 2D OBS30 SXH 4 25 1
+
+python bandstop_filter_decimate.py 2D OBS31 SXZ 3 9 1
+python bandpass_filter_decimate.py 2D OBS31 SXH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS32 BXZ 4 25 1
+python bandpass_filter_decimate.py 2D OBS32 LXH 4 25 1
+
+python bandstop_filter_decimate.py 2D OBS33 SXZ 3 9 1
+python bandpass_filter_decimate.py 2D OBS33 SXH 4 25 1
+
+python bandpass_filter_decimate.py 2D OBS34 SXZ 5 25 1
+python bandpass_filter_decimate.py 2D OBS34 SXH 4 25 1
+
+python bandpass_filter_decimate.py CI CIA BHZ 5 20 1
+python bandpass_filter_decimate.py CI SBI BHZ 5 20 1
+python bandpass_filter_decimate.py CI SCI2 BHZ 5 20 1
+python bandpass_filter_decimate.py CI SCZ2 BHZ 5 20 1
+python bandpass_filter_decimate.py CI SMI BHZ 5 20 1
+python bandpass_filter_decimate.py CI SNCC BHZ 5 20 1
+python bandpass_filter_decimate.py CI SRI BHZ 5 20 1
