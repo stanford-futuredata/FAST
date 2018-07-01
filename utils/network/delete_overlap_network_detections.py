b31c9d75b1580d2051f8652c53beab8250370bf9
ceyoon <ceyoon@dawn7.stanford.edu>
2018-06-30 17:26:02 -0700

    Added utils network and events files for OBSTest data set

--- utils/network/delete_overlap_network_detections.py
+++ utils/network/delete_overlap_network_detections.py
@@ -17,6 +17,10 @@ outfile_name = input_dir+'7sta_2stathresh_FinalUniqueNetworkDetectionTimes.txt'
 #allfile_name = input_dir+'uniquestart_sorted_no_duplicates.txt'
 #outfile_name = input_dir+'19sta_2stathresh_FinalUniqueNetworkDetectionTimes.txt'
 
+#input_dir = '/lfs/1/ceyoon/TimeSeries/OBSTest/network_detection/'
+#allfile_name = input_dir+'uniquestart_sorted_no_duplicates.txt'
+#outfile_name = input_dir+'35sta_2stathresh_FinalUniqueNetworkDetectionTimes.txt'
+
 # Output detection time at each station to file (avoid this if there are many stations)
 flag_each_station = False
 
