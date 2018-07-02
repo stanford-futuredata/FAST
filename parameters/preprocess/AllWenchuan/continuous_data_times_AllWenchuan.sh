61cd6b88731ab71704e4b08ff7c78c1f2b1a008a
ceyoon <ceyoon@dawn9.stanford.edu>
2018-07-02 16:44:58 -0700

    Added input parameters for preprocess continuous data, fingerprint, simsearch, postprocess for AllWenchuan data set

---
+++ parameters/preprocess/AllWenchuan/continuous_data_times_AllWenchuan.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+python get_continuous_data_times.py SN LUYA BHZ 
+python get_continuous_data_times.py SN LUYA BHN 
+python get_continuous_data_times.py SN LUYA BHE 
+
+python get_continuous_data_times.py SN MIAX BHZ 
+python get_continuous_data_times.py SN MIAX BHN 
+python get_continuous_data_times.py SN MIAX BHE 
+
+python get_continuous_data_times.py GS WDT BHZ 
+python get_continuous_data_times.py GS WDT BHN 
+python get_continuous_data_times.py GS WDT BHE 
+
+python get_continuous_data_times.py GS WXT BHZ 
+python get_continuous_data_times.py GS WXT BHN 
+python get_continuous_data_times.py GS WXT BHE 
+
+python get_continuous_data_times.py XX HSH BHZ 
+python get_continuous_data_times.py XX HSH BHN 
+python get_continuous_data_times.py XX HSH BHE 
+
+python get_continuous_data_times.py XX JJS BHZ 
+python get_continuous_data_times.py XX JJS BHN 
+python get_continuous_data_times.py XX JJS BHE 
+
+python get_continuous_data_times.py XX JMG BHZ 
+python get_continuous_data_times.py XX JMG BHN 
+python get_continuous_data_times.py XX JMG BHE 
+
+python get_continuous_data_times.py XX MXI BHZ 
+python get_continuous_data_times.py XX MXI BHN 
+python get_continuous_data_times.py XX MXI BHE 
+
+python get_continuous_data_times.py XX PWU BHZ 
+python get_continuous_data_times.py XX PWU BHN 
+python get_continuous_data_times.py XX PWU BHE 
+
+python get_continuous_data_times.py XX QCH BHZ 
+python get_continuous_data_times.py XX QCH BHN 
+python get_continuous_data_times.py XX QCH BHE 
+
+python get_continuous_data_times.py XX SPA BHZ 
+python get_continuous_data_times.py XX SPA BHN 
+python get_continuous_data_times.py XX SPA BHE 
+
+python get_continuous_data_times.py XX XCO BHZ 
+python get_continuous_data_times.py XX XCO BHN 
+python get_continuous_data_times.py XX XCO BHE 
+
+python get_continuous_data_times.py XX XJI BHZ 
+python get_continuous_data_times.py XX XJI BHN 
+python get_continuous_data_times.py XX XJI BHE 
+
+python get_continuous_data_times.py XX YGD BHZ 
+python get_continuous_data_times.py XX YGD BHN 
+python get_continuous_data_times.py XX YGD BHE 
+
+python get_continuous_data_times.py XX YZP BHZ 
+python get_continuous_data_times.py XX YZP BHN 
+python get_continuous_data_times.py XX YZP BHE 
+
---
+++ parameters/simsearch/AllWenchuan/run_simsearch_AllWenchuan.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Run similarity search on fingerprints
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh SN.LUYA BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh SN.LUYA BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh SN.LUYA BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh SN.MIAX BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh SN.MIAX BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh SN.MIAX BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh GS.WDT BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh GS.WDT BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh GS.WDT BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh GS.WXT BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh GS.WXT BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh GS.WXT BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.HSH BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.HSH BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.HSH BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.JJS BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.JJS BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.JJS BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.JMG BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.JMG BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.JMG BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.MXI BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.MXI BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.MXI BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.PWU BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.PWU BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.PWU BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.QCH BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.QCH BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.QCH BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.SPA BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.SPA BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.SPA BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.XCO BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.XCO BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.XCO BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.XJI BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.XJI BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.XJI BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.YGD BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.YGD BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.YGD BHE
+
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.YZP BHZ
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.YZP BHN
+../parameters/simsearch/AllWenchuan/filt_simsearch_input_AllWenchuan.sh XX.YZP BHE
+
