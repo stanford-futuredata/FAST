2ad78ab6d408c6543ec856fe37bc5d01a7f55a56
ceyoon <ceyoon@dawn7.stanford.edu>
2018-07-02 19:08:58 -0700

    Added preprocess bandpass and duration parameters for SaudiFull data set

---
+++ parameters/preprocess/SaudiFull/bandpass_filter_decimate_SaudiFull.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+python bandpass_filter_decimate.py SA EWJHS HHZ 4 20 2
+python bandpass_filter_decimate.py SA EWJHS HHN 4 20 2
+python bandpass_filter_decimate.py SA EWJHS HHE 4 20 2
+
+python bandpass_filter_decimate.py SA LNY01 HHZ 4 20 2
+python bandpass_filter_decimate.py SA LNY01 HHN 4 20 2
+python bandpass_filter_decimate.py SA LNY01 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA LNY02 HHZ 4 20 2
+python bandpass_filter_decimate.py SA LNY02 HHN 4 20 2
+python bandpass_filter_decimate.py SA LNY02 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA LNY03 HHZ 4 15 2
+python bandpass_filter_decimate.py SA LNY03 HHN 4 15 2
+python bandpass_filter_decimate.py SA LNY03 HHE 4 15 2
+
+python bandpass_filter_decimate.py SA LNY04 HHZ 4 20 2
+python bandpass_filter_decimate.py SA LNY04 HHN 4 20 2
+python bandpass_filter_decimate.py SA LNY04 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA LNY05 HHZ 4 20 2
+python bandpass_filter_decimate.py SA LNY05 HHN 4 20 2
+python bandpass_filter_decimate.py SA LNY05 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA LNY06 HHZ 4 15 2
+python bandpass_filter_decimate.py SA LNY06 HHN 4 15 2
+python bandpass_filter_decimate.py SA LNY06 HHE 4 15 2
+
+python bandpass_filter_decimate.py SA LNY07 HHZ 4 15 2
+python bandpass_filter_decimate.py SA LNY07 HHN 4 15 2
+python bandpass_filter_decimate.py SA LNY07 HHE 4 15 2
+
+python bandpass_filter_decimate.py SA LNY08 HHZ 4 15 2
+python bandpass_filter_decimate.py SA LNY08 HHN 4 15 2
+python bandpass_filter_decimate.py SA LNY08 HHE 4 15 2
+
+python bandpass_filter_decimate.py SA LNY09 HHZ 5 15 2
+python bandpass_filter_decimate.py SA LNY09 HHN 5 15 2
+python bandpass_filter_decimate.py SA LNY09 HHE 5 15 2
+
+python bandpass_filter_decimate.py SA LNY10 HHZ 4 12 2
+python bandpass_filter_decimate.py SA LNY10 HHN 4 12 2
+python bandpass_filter_decimate.py SA LNY10 HHE 4 12 2
+
+python bandpass_filter_decimate.py SA LNY11 HHZ 4 20 2
+python bandpass_filter_decimate.py SA LNY11 HHN 4 20 2
+python bandpass_filter_decimate.py SA LNY11 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA LNY12 HHZ 4 16 2
+python bandpass_filter_decimate.py SA LNY12 HHN 4 16 2
+python bandpass_filter_decimate.py SA LNY12 HHE 4 16 2
+
+python bandpass_filter_decimate.py SA LNY13 HHZ 4 10 2
+python bandpass_filter_decimate.py SA LNY13 HHN 4 10 2
+python bandpass_filter_decimate.py SA LNY13 HHE 4 10 2
+
+python bandpass_filter_decimate.py SA LNY14 HHZ 4 16 2
+python bandpass_filter_decimate.py SA LNY14 HHN 4 16 2
+python bandpass_filter_decimate.py SA LNY14 HHE 4 16 2
+
+python bandpass_filter_decimate.py SA LNY15 HHZ 6 16 2
+python bandpass_filter_decimate.py SA LNY15 HHN 6 16 2
+python bandpass_filter_decimate.py SA LNY15 HHE 6 16 2
+
+python bandpass_filter_decimate.py SA LNY16 HHZ 4 15 2
+python bandpass_filter_decimate.py SA LNY16 HHN 4 15 2
+python bandpass_filter_decimate.py SA LNY16 HHE 4 15 2
+
+python bandpass_filter_decimate.py SA LNY17 HHZ 6 12 2
+python bandpass_filter_decimate.py SA LNY17 HHN 6 12 2
+python bandpass_filter_decimate.py SA LNY17 HHE 6 12 2
+
+python bandpass_filter_decimate.py SA LNYS HHZ 4 12 2
+python bandpass_filter_decimate.py SA LNYS HHN 4 12 2
+python bandpass_filter_decimate.py SA LNYS HHE 4 12 2
+
+python bandpass_filter_decimate.py SA MURBA HHZ 4 16 2
+python bandpass_filter_decimate.py SA MURBA HHN 4 16 2
+python bandpass_filter_decimate.py SA MURBA HHE 4 16 2
+
+python bandpass_filter_decimate.py SA NUMJS HHZ 4 16 2
+python bandpass_filter_decimate.py SA NUMJS HHN 4 16 2
+python bandpass_filter_decimate.py SA NUMJS HHE 4 16 2
+
+python bandpass_filter_decimate.py SA OLAS HHZ 4 20 2
+python bandpass_filter_decimate.py SA OLAS HHN 4 20 2
+python bandpass_filter_decimate.py SA OLAS HHE 4 20 2
+
+python bandpass_filter_decimate.py SA SUMJS HHZ 4 15 2
+python bandpass_filter_decimate.py SA SUMJS HHN 4 15 2
+python bandpass_filter_decimate.py SA SUMJS HHE 4 15 2
+
+python bandpass_filter_decimate.py SA UMJ01 HHZ 4 20 2
+python bandpass_filter_decimate.py SA UMJ01 HHN 4 20 2
+python bandpass_filter_decimate.py SA UMJ01 HHE 4 20 2
+python bandpass_filter_decimate.py SA UMJ02 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA UMJ03 HHZ 4 15 2
+python bandpass_filter_decimate.py SA UMJ03 HHN 4 15 2
+python bandpass_filter_decimate.py SA UMJ03 HHE 4 15 2
+
+python bandpass_filter_decimate.py SA UMJ04 HHZ 4 20 2
+python bandpass_filter_decimate.py SA UMJ04 HHN 4 20 2
+python bandpass_filter_decimate.py SA UMJ04 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA UMJ05 HHZ 4 16 2
+python bandpass_filter_decimate.py SA UMJ05 HHN 4 16 2
+python bandpass_filter_decimate.py SA UMJ05 HHE 4 16 2
+
+python bandpass_filter_decimate.py SA UMJ06 HHZ 4 20 2
+python bandpass_filter_decimate.py SA UMJ06 HHN 4 20 2
+python bandpass_filter_decimate.py SA UMJ06 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA UMJ07 HHZ 4 20 2
+python bandpass_filter_decimate.py SA UMJ07 HHN 4 20 2
+python bandpass_filter_decimate.py SA UMJ07 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA UMJ08 HHZ 4 20 2
+python bandpass_filter_decimate.py SA UMJ08 HHN 4 20 2
+python bandpass_filter_decimate.py SA UMJ08 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA UMJ09 HHZ 4 20 2
+python bandpass_filter_decimate.py SA UMJ09 HHN 4 20 2
+python bandpass_filter_decimate.py SA UMJ09 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA UMJ10 HHZ 4 20 2
+python bandpass_filter_decimate.py SA UMJ10 HHN 4 20 2
+python bandpass_filter_decimate.py SA UMJ10 HHE 4 20 2
+
+python bandpass_filter_decimate.py SA UMJ11 HHZ 4 16 2
+python bandpass_filter_decimate.py SA UMJ11 HHN 4 16 2
+python bandpass_filter_decimate.py SA UMJ11 HHE 4 16 2
+
+python bandpass_filter_decimate.py SA UMJ12 HHZ 4 16 2
+python bandpass_filter_decimate.py SA UMJ12 HHN 4 16 2
+python bandpass_filter_decimate.py SA UMJ12 HHE 4 16 2
+
+python bandpass_filter_decimate.py SA UMJS HHZ 4 10 2
+python bandpass_filter_decimate.py SA UMJS HHN 4 10 2
+python bandpass_filter_decimate.py SA UMJS HHE 4 10 2
+
