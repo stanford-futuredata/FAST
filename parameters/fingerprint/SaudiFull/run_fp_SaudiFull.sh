29f453e247e85cda073fdba613616f900270cf1b
ceyoon <ceyoon@dawn7.stanford.edu>
2018-07-02 19:31:31 -0700

    Added fingerprint, simsearch, postprocess parameters for SaudiFull data set

---
+++ parameters/fingerprint/SaudiFull/run_fp_SaudiFull.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+# Create fingerprints
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_EWJHS_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_EWJHS_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_EWJHS_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY01_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY01_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY01_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY02_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY02_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY02_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY03_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY03_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY03_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY04_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY04_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY04_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY05_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY05_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY05_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY06_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY06_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY06_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY07_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY07_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY07_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY08_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY08_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY08_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY09_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY09_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY09_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY10_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY10_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY10_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY11_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY11_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY11_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY12_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY12_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY12_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY13_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY13_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY13_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY14_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY14_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY14_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY15_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY15_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY15_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY16_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY16_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY16_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY17_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY17_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNY17_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNYS_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNYS_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_LNYS_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_MURBA_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_MURBA_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_MURBA_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_NUMJS_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_NUMJS_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_NUMJS_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_OLAS_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_OLAS_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_OLAS_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_SUMJS_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_SUMJS_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_SUMJS_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ01_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ01_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ01_HHE.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ02_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ03_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ03_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ03_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ04_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ04_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ04_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ05_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ05_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ05_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ06_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ06_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ06_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ07_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ07_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ07_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ08_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ08_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ08_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ09_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ09_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ09_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ10_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ10_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ10_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ11_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ11_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ11_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ12_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ12_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJ12_HHE.json
+
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJS_HHZ.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJS_HHN.json
+python gen_fp.py ../parameters/fingerprint/SaudiFull/fp_input_SA_UMJS_HHE.json
+
+# Global index: after all fingerprints are done
+python global_index.py ../parameters/fingerprint/SaudiFull/global_indices_SaudiFull.json
---
+++ parameters/simsearch/SaudiFull/run_simsearch_SaudiFull.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# Run similarity search on fingerprints
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh EWJHS HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh EWJHS HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh EWJHS HHE
+
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY01 HHZ
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY01 HHN
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY01 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY02 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY02 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY02 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY03 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY03 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY03 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY04 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY04 HHN
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY04 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY05 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY05 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY05 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY06 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY06 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY06 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY07 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY07 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY07 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY08 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY08 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY08 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY09 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY09 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY09 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY10 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY10 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY10 HHE
+
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY11 HHZ
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY11 HHN
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY11 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY12 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY12 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY12 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY13 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY13 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY13 HHE
+
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY14 HHZ
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY14 HHN
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh LNY14 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY15 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY15 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY15 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY16 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY16 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY16 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY17 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY17 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNY17 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNYS HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNYS HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh LNYS HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh MURBA HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh MURBA HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh MURBA HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh NUMJS HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh NUMJS HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh NUMJS HHE
+
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh OLAS HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh OLAS HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh OLAS HHE
+
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh SUMJS HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh SUMJS HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh SUMJS HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ01 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ01 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ01 HHE
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ02 HHE
+
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh UMJ03 HHZ
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh UMJ03 HHN
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh UMJ03 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ04 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ04 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ04 HHE
+
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh UMJ05 HHZ
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh UMJ05 HHN
+../parameters/simsearch/SaudiFull/filt_simsearch_input_SaudiFull.sh UMJ05 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ06 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ06 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ06 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ07 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ07 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ07 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ08 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ08 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ08 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ09 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ09 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ09 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ10 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ10 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ10 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ11 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ11 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ11 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ12 HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ12 HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJ12 HHE
+
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJS HHZ
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJS HHN
+../parameters/simsearch/SaudiFull/simsearch_input_SaudiFull.sh UMJS HHE
+
