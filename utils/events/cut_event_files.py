23ecdf7ac6740d82aa97172f7517aafc31f6c143
ttapparo <talisetapparo@aol.com>
2022-09-02 09:26:31 -0700

    working on cut file script

--- utils/events/cut_event_files.py
+++ utils/events/cut_event_files.py
@@ -1,10 +1,7 @@
-from struct import unpack
 from time import time
 from obspy import read
 from obspy import Stream
 from obspy import UTCDateTime
-from obspy.clients.fdsn import Client 
-from obspy.clients.fdsn.header import FDSNException
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
@@ -12,9 +9,6 @@ import sys
 import os
 import json
 import glob
-import collections
-
-EQ = collections.namedtuple("EQ", ["ev_id_list", "det_time", "det_start_ind", "det_end_ind", "dL", "diff_ind", "nsta", "peaksum"])
 
 # Inputs
 stations = ['CDY', 'CPM', 'GTM', 'HEC', 'RMM', 'RMR', 'TPC']
@@ -35,10 +29,16 @@ wtime_after = 120 # time window after origin time (s)
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
+ev_ids = []
+det_times = []
+det_start_times = []
+det_end_times = []
 dt_fp = 1.0
 det_times = []
 diff_times = []
 dL_dt = []
+nstas = []
+peaksums = []
 
 with open(in_FINAL_Detection_List, 'r') as f:
     EQ_data = f.readlines()[:]
@@ -47,23 +47,17 @@ EQ_detections = []
 
 for data in EQ_data:
     line = data.split()
-    ev_id = line[0]
-    det_time = line[1]
-    det_start_ind = line[2]
-    det_end_ind = line[3]
+    ev_ids.append(line[0])
+    det_times.append(line[1])
+    det_start_times.append(line[2])
+    det_end_times.append(line[3])
     dL = line[4]
     diff_ind = line[5]
-    nsta = line[6]
-    peaksum = line[7]
-
+    nstas.append(line[6])
+    peaksums.append(line[7])
     det_times.append(int(det_start_ind) * dt_fp)
     diff_times.append(int(diff_ind) * dt_fp)
     dL_dt.append(int(dL) * dt_fp)
-
-    # print(f"ev_id: {ev_id}\ndet_time: {det_time}\ndet_start)ind: {det_start_ind}\ndet_end_ind: {det_end_ind}\ndL: {dL}\ndiff_ind: {diff_ind}\nnsta: {nsta}\npeaksum: {peaksum}")
-
-    EQ_detection = EQ(ev_id, det_time, det_start_ind, det_end_ind, dL, diff_ind, nsta, peaksum)
-    EQ_detections.append(EQ_detection)
 
 st = read('waveforms*/Deci5.Pick.*', format='SAC')
 print(len(st))
@@ -73,8 +67,8 @@ print("\n ------------------- OUTPUT CUT EVENT FILES --------------------------\
 
 i_load = 0
 
-for kk in range(len(EQ_detections)):
-    curr_ev = EQ_detections[kk].ev_id_list
+for kk in range(len(ev_ids)):
+    curr_ev = ev_ids[kk]
 
     ev_time = init_time + det_times[kk]
     start_time = ev_time - wtime_before
