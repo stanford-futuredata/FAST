2038d64b13c84378fde6633132aa0f8dd63ad6ea
ttapparo <talisetapparo@aol.com>
2022-10-03 12:38:25 -0700

    calipatria dataset scripts

---
+++ parameters/postprocess/output_calipatria_pairs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_TONN_HNE -i ../data/global_indices/TONN_HNE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_TONN_HNN -i ../data/global_indices/TONN_HNN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_TONN_HNZ -i ../data/global_indices/TONN_HNZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_BOM_HHZ -i ../data/global_indices/BOM_HHZ_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_BOM_HHE -i ../data/global_indices/BOM_HHE_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_CLI2_EHZ -i ../data/global_indices/CLI2_EHZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_CRR_HHE -i ../data/global_indices/CRR_HHE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_CRR_HHN -i ../data/global_indices/CRR_HHN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_CRR_HHZ -i ../data/global_indices/CRR_HHZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_DRE_HHE -i ../data/global_indices/DRE_HHE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_DRE_HHN -i ../data/global_indices/DRE_HHN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_DRE_HHZ -i ../data/global_indices/DRE_HHZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_FRK_HHE -i ../data/global_indices/FRK_HHE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_FRK_HHN -i ../data/global_indices/FRK_HHN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_FRK_HHZ -i ../data/global_indices/FRK_HHZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_IMP_HHE -i ../data/global_indices/IMP_HHE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_IMP_HHN -i ../data/global_indices/IMP_HHN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_IMP_HHZ -i ../data/global_indices/IMP_HHZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_286_HNE -i ../data/global_indices/286_HNE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_286_HNN -i ../data/global_indices/286_HNN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_286_HNZ -i ../data/global_indices/286_HNZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_5274_HNE -i ../data/global_indices/5274_HNE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_5274_HNN -i ../data/global_indices/5274_HNN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_5274_HNZ -i ../data/global_indices/5274_HNZ_idx_mapping.txt 
+
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_WLA06_HNE -i ../data/global_indices/WLA06_HNE_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_WLA06_HNN -i ../data/global_indices/WLA06_HNN_idx_mapping.txt 
+python parse_results.py -d ../data/waveforms_mdl_BP/fingerprints/ -p candidate_pairs_WLA06_HNZ -i ../data/global_indices/WLA06_HNZ_idx_mapping.txt 
+ 
---
+++ utils/preprocess/mdl_bandpass_filter.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+python bandpass_filter_decimate.py AZ TONN HNZ 5 12 5
+python bandpass_filter_decimate.py AZ TONN HNN 5 12 5
+python bandpass_filter_decimate.py AZ TONN HNE 5 12 5
+
+python bandpass_filter_decimate.py CI BOM HHZ 5 12 5
+python bandpass_filter_decimate.py CI BOM HHE 5 12 5
+
+python bandpass_filter_decimate.py CI CLI2 EHZ 5 12 5
+
+python bandpass_filter_decimate.py CI CRR HHZ 5 12 5
+python bandpass_filter_decimate.py CI CRR HHN 5 12 5
+python bandpass_filter_decimate.py CI CRR HHE 5 12 5
+
+python bandpass_filter_decimate.py CI DRE HHZ 5 12 5
+python bandpass_filter_decimate.py CI DRE HHN 5 12 5
+python bandpass_filter_decimate.py CI DRE HHE 5 12 5
+
+python bandpass_filter_decimate.py CI FRK HHZ 5 12 5
+python bandpass_filter_decimate.py CI FRK HHN 5 12 5
+python bandpass_filter_decimate.py CI FRK HHE 5 12 5
+
+python bandpass_filter_decimate.py CI IMP HHZ 5 12 5
+python bandpass_filter_decimate.py CI IMP HHN 5 12 5
+python bandpass_filter_decimate.py CI IMP HHE 5 12 5
+
+python bandpass_filter_decimate.py CI NSS2 HHZ 5 12 5
+python bandpass_filter_decimate.py CI NSS2 HHN 5 12 5
+python bandpass_filter_decimate.py CI NSS2 HHE 5 12 5
+
+python bandpass_filter_decimate.py CI SWS HHZ 5 12 5
+python bandpass_filter_decimate.py CI SWS HHN 5 12 5
+python bandpass_filter_decimate.py CI SWS HHE 5 12 5
+
+python bandpass_filter_decimate.py NP 286 HNZ 5 12 5
+python bandpass_filter_decimate.py NP 286 HNN 5 12 5
+python bandpass_filter_decimate.py NP 286 HNE 5 12 5
+
+python bandpass_filter_decimate.py NP 5274 HNZ 5 12 5
+python bandpass_filter_decimate.py NP 5274 HNN 5 12 5
+python bandpass_filter_decimate.py NP 5274 HNE 5 12 5
+
+python bandpass_filter_decimate.py SB WLA06 HNZ 5 12 5
+python bandpass_filter_decimate.py SB WLA06 HNN 5 12 5
+python bandpass_filter_decimate.py SB WLA06 HNE 5 12 5
+
